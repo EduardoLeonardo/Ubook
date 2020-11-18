66211223eb0bfa573d5360dacd8f6583e121944a
Eduardo Leonardo <eduardoleonardo1@hotmail.com>
2020-11-18 19:25:17 -0300

    feat(list-contact): consulta ao contact serviço e exibição dos contatos

--- src/app/components/list-contact/list-contact.component.ts
+++ src/app/components/list-contact/list-contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from 'src/app/domain/contact';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-list-contact',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private contactService: ContactService) { }
+
+  contactList: Contact[];
 
   ngOnInit() {
+    this.contactList = this.contactService.getContactList();
   }
 
 }
