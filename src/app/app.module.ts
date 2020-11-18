c1ed800cc6f0a85f909f59b783e7ceedeb12f830
Eduardo Leonardo <eduardoleonardo1@hotmail.com>
2020-11-18 18:33:53 -0300

    feat(module): ajustes no module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactService } from './services/contact.service';
+import { EditContactModalComponent } from './components/edit-contact-modal/edit-contact-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditContactModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    ContactService
+  ],
+  exports:[EditContactModalComponent],
+  entryComponents: [EditContactModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
