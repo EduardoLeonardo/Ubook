6d0ea37b1ad172cbf8d2830cf523345101866f99
Eduardo Leonardo <eduardoleonardo1@hotmail.com>
2020-11-19 17:29:15 -0300

    feat(button): criação do componente button para criação de contatos

---
+++ src/app/components/new-contact-button/new-contact-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EditContactModalComponent } from '../edit-contact-modal/edit-contact-modal.component';
+
+@Component({
+  selector: 'app-new-contact-button',
+  templateUrl: './new-contact-button.component.html',
+  styleUrls: ['./new-contact-button.component.css']
+})
+export class NewContactButtonComponent implements OnInit {
+
+
+  @Output() saveContact: EventEmitter<void> = new EventEmitter<any>();
+  constructor(private modalService: NgbModal) { }
+
+  ngOnInit() {
+  }
+
+
+  openModal() {
+    const modalRef = this.modalService.open(EditContactModalComponent);
+    modalRef.result.then(() => this.saveContact.emit());
+  }
+}
